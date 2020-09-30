514de1cb77cba8e633c7df4f027d44eeed6575f2
Amulya Prodduturi <amulyareddyp15@gmail.com>
2020-09-30 17:47:53 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,31 +4,20 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget : [
-    {
-        title : 'Eat out',
-        budget : 30
-    },
-    {
-        title : 'Rent',
-        budget : 350
-    },
-    {
-        title : 'Groceries',
-        budget : 90
-    },
-]
-};
+const budget = require('./myBudget.json');
+console.log(budget);
 
 app.get('/hello', (req,res) => {
     res.send("Hello World!");
 });
 
-app.get('/budget', (req,res) => {
+app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
     console.log("Example app listening at http://localhost:3000");
 });
+
+
+
