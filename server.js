20bcca828675732136e36c4e9aaa29f32687cb14
Amulya Prodduturi <amulyareddyp15@gmail.com>
2020-10-30 11:23:57 -0400

    Fetching data from MongoDB and added end points

--- server.js
+++ server.js
@@ -1,21 +1,60 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const mongoose  = require('mongoose');
+const cors = require('cors');
+const budgetModel = require('./models/budgetModel');
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var url = 'mongodb://localhost:27017/budget_info';
+app.use('',express.static('public'));
 app.use(cors());
 
-const budget = require('./myBudget.json');
-console.log(budget);
-
-
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
-
-app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+app.get('/hello',(req,res)=>{
+    res.send("sample text");    
 });
 
 
+app.get('/budget',(req,res)=>{
+   
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection is established");
+                budgetModel.find({})
+                           .then((data)=>{
+                               console.log(data);
+                               res.status(200).send(data);
+                               mongoose.connection.close();
+                           })
+                           .catch((err)=>{
+                               console.log(err);
+                               res.status(500).send();
+                           })
+            })
+})
+
+
+app.post('/budget',(req,res)=>{
+    console.log(req.body);
+    let data = {id: req.body._id, title: req.body.title, budget: req.body.budget, color: req.body.color}
+    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(()=>{
+                console.log("Connection is established");
+                budgetModel.insertMany(data,(err,data)=>{
+                    if(err){
+                        console.log(err);                        
+                    }else{
+                        console.log("Data is inserted"); 
+                        res.send(data);    
+                        mongoose.disconnect();
+                    }                    
+                })                              
+})
+});
+
+app.listen(port,()=>{
+    console.log("App is running on port "+port);
+});
